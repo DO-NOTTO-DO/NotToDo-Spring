afa1e88c0171247185f33833a664a3c231845a68
jeonghoon <shb03323@naver.com>
2023-01-02 00:38:41 +0900

    feat: RecommendCategory model 생성

---
+++ src/main/java/sopt/nottodo/domain/RecommendCategory.java
@@ -0,0 +1,24 @@
+package sopt.nottodo.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecommendCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column
+    private String image;
+}
