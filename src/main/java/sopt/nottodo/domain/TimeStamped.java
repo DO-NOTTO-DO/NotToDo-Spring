60299bbe40b1d5caa53cd9537392547f26d2dadf
jeonghoon <shb03323@naver.com>
2023-01-01 23:58:41 +0900

    feat: timestamp 생성

---
+++ src/main/java/sopt/nottodo/domain/TimeStamped.java
@@ -0,0 +1,22 @@
+package nottodo.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class TimeStamped {
+
+    @CreatedDate
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+}
