779f1b201dec2de36654da9175931f4dc5eb4ad1
jeonghoon <shb03323@naver.com>
2023-01-10 17:24:23 +0900

    feat: #1 컨트롤러 제작

---
+++ src/main/java/sopt/nottodo/controller/AuthController.java
@@ -0,0 +1,29 @@
+package sopt.nottodo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sopt.nottodo.dto.auth.LoginRequest;
+import sopt.nottodo.service.AuthService;
+import sopt.nottodo.util.response.ResponseCode;
+import sopt.nottodo.util.response.ResponseDataMessage;
+import sopt.nottodo.util.response.ResponseMessage;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login/{social}")
+    public ResponseEntity<ResponseMessage> socialLogin(@PathVariable String social, @Valid @RequestBody LoginRequest request) {
+        return ResponseDataMessage.toResponseEntity(
+                ResponseCode.LOGIN_SUCCESS,
+                authService.socialLogin(social, request)
+        );
+    }
+}
