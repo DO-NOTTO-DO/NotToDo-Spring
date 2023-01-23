53ecbfd23133b2af2cf8f9e0539655dd39b0304d
jeonghoon <shb03323@naver.com>
2023-01-23 18:30:45 +0900

    chore: 날짜 포맷 변수명 변경

--- src/main/java/sopt/nottodo/util/DateModule.java
+++ src/main/java/sopt/nottodo/util/DateModule.java
@@ -14,11 +14,11 @@ import java.util.Date;
 public class DateModule {
 
     private static final Integer MONDAY = 1;
-    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String DATE_DASH_FORMAT = "yyyy-MM-dd";
 
     public static Date getToday(String today) {
         try {
-            SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT);
+            SimpleDateFormat format = new SimpleDateFormat(DATE_DASH_FORMAT);
             return format.parse(today);
         } catch (ParseException e) {
             throw new CustomException(ResponseCode.INVALID_DATE_FORMAT);
